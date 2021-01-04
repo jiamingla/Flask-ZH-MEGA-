7cbf6e1a6c09b73554987b4703de34297620eb10
Your Name <you@example.com>
2021-01-04 20:33:04 +0800

    make it better

--- app/models.py
+++ app/models.py
@@ -57,7 +57,7 @@ followers = db.Table('followers',
 )
 
 #使用者的表模型
-class user(UserMixin, db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -66,13 +66,13 @@ class user(UserMixin, db.Model):
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     followed = db.relationship(
-        'user', secondary=followers,
+        'User', secondary=followers,
         primaryjoin=(followers.c.follower_id == id),
         secondaryjoin=(followers.c.followed_id == id),
         backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):
-        return '<user {}>'.format(self.username)
+        return '<User {}>'.format(self.username)
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
     def check_password(self, password):
@@ -122,7 +122,7 @@ class user(UserMixin, db.Model):
 def load_user(id):
     return User.query.get(int(id))
 #PO文的表
-class post(SearchableMixin, db.Model):
+class Post(SearchableMixin, db.Model):
     __searchable__ = ['body']
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -133,4 +133,4 @@ class post(SearchableMixin, db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     language = db.Column(db.String(5))
     def __repr__(self):
-        return '<post {}>'.format(self.body)
+        return '<Post {}>'.format(self.body)
